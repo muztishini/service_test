2bf250856ea5143906ceb6cbac82c0a2414d98d9
muztishini <a.kroshakov@yandex.ru>
2022-12-20 16:41:14 +0500

    change3

--- servicetest/tests/urls.py
+++ servicetest/tests/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 from . import views
 from .views import *
-# from django.conf.urls import url
-
-# app_name = 'tests'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('detail/<int:kits_id>/', views.detail, name='detail'),
     path('answer/<int:kits_id>/<int:riddle_id>/', views.answer, name='answer'),
-    path('register/', RegisterUser.as_view(), name='register')
-    
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('logout/', logout_user, name='logout'),
+    path('login/', LoginUser.as_view(), name='login')
 ]
--- servicetest/tests/views.py
+++ servicetest/tests/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.views import LoginView
 from .models import Riddle, Option, Kits
 from django.contrib.auth import logout, login
 from django.contrib.auth.decorators import login_required
@@ -11,12 +13,13 @@ def index(request):
     return render(request, "index.html", {"list_kits": Kits.objects.order_by('id')})
 
 
+@login_required
 def detail(request, kits_id):
     dl = []
     det_lst = Riddle.objects.order_by('id')
-    for i in range(kits_id*3-3, kits_id*3):
+    for i in range(kits_id * 3 - 3, kits_id * 3):
         dl.append(det_lst[i])
-    ki = kits_id*3-2
+    ki = kits_id * 3 - 2
     context = {"detail_list": dl,
                "num_kits": kits_id,
                "ki": ki}
@@ -32,15 +35,15 @@ def answer(request, riddle_id, kits_id):
     global count_v, count_n
     # count_riddle = Riddle.objects.all().count()
 
-    if riddle_id <= kits_id*3:
+    if riddle_id <= kits_id * 3:
         riddle = get_object_or_404(Riddle, pk=riddle_id)
         kits = get_object_or_404(Kits, pk=kits_id)
         try:
             option = riddle.option_set.get(pk=request.POST['option'])
-            
+
         except (KeyError, Option.DoesNotExist):
             return render(request, 'answer.html', {'kits': kits, 'riddle': riddle})
-           
+
         else:
             if option.correct:
                 count_v += 1
@@ -53,7 +56,7 @@ def answer(request, riddle_id, kits_id):
                 return HttpResponseRedirect(f"/answer/{kits_id}/{riddle_id + 1}")
 
     if count_n != 0:
-        perc = round((count_v/(count_v+count_n))*100, 2)
+        perc = round((count_v / (count_v + count_n)) * 100, 2)
     else:
         perc = 100
     context = {'message_v': count_v, 'message_n': count_n, 'perc': perc}
@@ -65,15 +68,33 @@ def answer(request, riddle_id, kits_id):
 class RegisterUser(CreateView):
     form_class = UserCreationForm
     template_name = 'register.html'
-        
+
     def get(self, request):
         context = {
-            'form' : self.form_class
+            'form': self.form_class
         }
         return render(request, self.template_name, context)
-    
+
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
         return redirect('index')
-    
+
+
+class LoginUser(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'login.html'
+
+    def get(self, request):
+        context = {
+            'form': self.form_class
+        }
+        return render(request, self.template_name, context)
+
+    def get_success_url(self):
+        return reverse_lazy('index')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
